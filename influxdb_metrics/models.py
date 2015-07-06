f549379e1865e6815aaad311a7f979ffe51494c4
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2015-07-06 11:04:18 +0200

    Migrated model signals to custom user model

--- influxdb_metrics/models.py
+++ influxdb_metrics/models.py
@@ -1,5 +1,6 @@
 """Models and signal handlers for the influxdb_metrics app."""
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.signals import user_logged_in
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
@@ -12,18 +13,18 @@ def user_logged_in_handler(sender, **kwargs):
     write_point('default.django.auth.user.login', value=1)
 
 
-@receiver(post_delete, sender=User)  # pragma: no cover
 def user_post_delete_handler(sender, **kwargs):
     """Sends a metric to InfluxDB when a User object is deleted."""
-    total = User.objects.all().count()
+    total = get_user_model().objects.all().count()
     write_point('default.django.auth.user.delete', value=1)
     write_point('default.django.auth.user.count', value=total)
+post_delete.connect(user_post_delete_handler, sender=settings.AUTH_USER_MODEL)
 
 
-@receiver(post_save, sender=User)  # pragma: no cover
-def user_post_save_handler(sender, **kwargs):
+def user_post_save_handler(**kwargs):
     """Sends a metric to InfluxDB when a new User object is created."""
     if kwargs.get('created'):
-        total = User.objects.all().count()
+        total = get_user_model().objects.all().count()
         write_point('default.django.auth.user.create', value=1)
         write_point('default.django.auth.user.count', value=total)
+post_save.connect(user_post_save_handler, sender=settings.AUTH_USER_MODEL)
